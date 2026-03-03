b63a6e1c978e4d2c2a783832a7a7c6d92c25ba2d
Liz Thompson <elth8461@colorado.edu>
2026-03-03 10:18:03 -0700

    Update testMain.m

--- testMain.m
+++ testMain.m
@@ -1,13 +1,13 @@
 clear; clc; close all;
 
-load('RSdata_nocontrol.mat')
+load('RSdata_nocontrol.mat') % load porvided data to test plotting
 
 time = rt_estim.time;
 aircraft_state_array = rt_estim.signals.values;
 control_input_array = rt_motor.signals.values;
-fig = 1:6;
-col = ['r','b','g','m','c','y'];
+fig = 1:6; % set figure numbers
+col = ['r','b','g','m','c','k']; % set colors for plotting, groups by translational and rotational motion/forces
 
 
-
+% Test plotting
 PlotAircraftSim(time,aircraft_state_array,control_input_array,fig, col)
