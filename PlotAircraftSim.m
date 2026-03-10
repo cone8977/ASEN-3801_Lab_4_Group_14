b6b96a594aae81e1accf7578c5bb7577fdc6813e
cone8977 <130427347+cone8977@users.noreply.github.com>
2026-03-10 11:55:35 -0600

    Added Figure labeling
    Dynamic Figure labeling consistent with our convention. New input Task put in what task and question these figures are for. For example for task 1 question 4 put 14

--- PlotAircraftSim.m
+++ PlotAircraftSim.m
@@ -3,14 +3,14 @@
 % File name: PlotAircraftSim
 % Created: 3/2/26
 
-function PlotAircraftSim(time,aircraft_state_array,control_input_array,fig, col)
+function PlotAircraftSim(time,aircraft_state_array,control_input_array,fig, col,Task)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Inputs:   time = Time corresponding to the nth set of variables
 %           aircraft_state_array = 12 x n array of aircraft states
 %           control_input_array = 4 x n array of control inputs (Zc,Lc,Mc,Nc)
 %           fig = 6x1 vector of figure numbers to plot over
 %           col = string indicating the plotting option for each plot
-%                           
+%           Task = what Task and part for figure labeling                 
 % Outputs:  NO NUMERICAL. Only plots the following 6 figures:
 %               - Four figures each with 3 subplots for intertial position,
 %               euler angles, intertial velocity, and angular velocity
@@ -45,7 +45,7 @@ subplot(3,1,2); plot(time,y,col(2),LineWidth=1); grid on;
 ylabel('Y-Position (m)'); xlim([0,max(time)]);
 subplot(3,1,3); plot(time,z,col(3),LineWidth=1); grid on;
 ylabel('Z-Position (m)'); xlim([0,max(time)]); xlabel('Time (s)');
-
+print(sprintf('ASEN3801_L4_%sa',Task),'-dpng','-r300')
 figure(fig(2)); % Figure 2 for A/C Euler Angles
 hold on; grid on;
 sgtitle('3-2-1 Body Euler Angles vs Time')
@@ -55,7 +55,7 @@ subplot(3,1,2); plot(time,theta,col(5),LineWidth=1); grid on;
 ylabel('Pitch Angle (rad)'); xlim([0,max(time)]);
 subplot(3,1,3); plot(time,psi,col(6),LineWidth=1); grid on;
 ylabel('Yaw Angle (rad)'); xlim([0,max(time)]); xlabel('Time (s)');
-
+print(sprintf('ASEN3801_L4_%sb',Task),'-dpng','-r300')
 figure(fig(3)); % Figure 3 for A/C Air Relative Velocity
 hold on; grid on;
 sgtitle('Air-Relative Velocity Components')
@@ -65,7 +65,7 @@ subplot(3,1,2); plot(time,v,col(2),LineWidth=1); grid on;
 ylabel('Y-Velocity (m/s)'); xlim([0,max(time)]);
 subplot(3,1,3); plot(time,w,col(3),LineWidth=1); grid on;
 ylabel('Z-Velocity (m/s)'); xlim([0,max(time)]); xlabel('Time (s)');
-
+print(sprintf('ASEN3801_L4_%sc',Task),'-dpng','-r300')
 figure(fig(4)); % Figure 4 for A/C Angular Rates
 hold on; grid on;
 sgtitle('3-2-1 Body Angular Rates')
@@ -75,7 +75,7 @@ subplot(3,1,2); plot(time,q,col(5),LineWidth=1);  grid on;
 ylabel('Pitch Rate (rad/s)'); xlim([0,max(time)]);
 subplot(3,1,3); plot(time,r,col(6),LineWidth=1); grid on;
 ylabel('Yaw Rate (rad/s)'); xlim([0,max(time)]); xlabel('Time (s)');
-
+print(sprintf('ASEN3801_L4_%sd',Task),'-dpng','-r300')
 %% Plotting Control Inputs
 
 figure(fig(5)); % Figure 5 for A/C Control Inputs
@@ -89,7 +89,7 @@ subplot(4,1,3); plot(time,Mc,col(5),LineWidth=1);
 ylabel('Y-Moment'); xlim([0,max(time)]); grid on;
 subplot(4,1,4); plot(time,Nc,col(6),LineWidth=1); 
 ylabel('Z-Moment'); xlim([0,max(time)]); xlabel('Time (s)'); grid on;
-
+print(sprintf('ASEN3801_L4_%se',Task),'-dpng','-r300')
 %% Plotting 3D Trajectory 
 
 figure(fig(6)); % Figure 6 for A/C 3D Trajectory
@@ -98,5 +98,5 @@ title('3D Trajectory')
 plot3(x,y,-z,'k',LineWidth=1); % 3D Trajectory
 plot3(x(1),y(1),-z(1),'go'); plot3(x(end),y(end),-z(end),'ro') % Markers
 xlabel('X-Positon (m)'); ylabel('Y-Position (m)'); zlabel('Altitude (m)')
-
+print(sprintf('ASEN3801_L4_%sf',Task),'-dpng','-r300')
 end
