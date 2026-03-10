f69761dbbb66e6bee416179b5a17499114b484c0
Liz Thompson <elth8461@colorado.edu>
2026-03-10 15:00:57 -0600

    Update QuadrotorEOM_Linearized.m

--- QuadrotorEOM_Linearized.m
+++ QuadrotorEOM_Linearized.m
@@ -1,66 +1,62 @@
+% Contributors: Victor Turpin, Liz Thompson
+% Course number: ASEN 3801
+% File name: QuadrotorEOM
+% Created: 3/10/26
+
 function var_dot = QuadrotorEOM_Linearized(t, var, g, m, I, deltaFc, deltaGc)
-% for use by ode45 to simulate the linear equations of motion where: t is time; var is the 12 x
-% 1 aircraft state vector; g is the acceleration due to gravity; m is mass; I is the inertia matrix;
-% deltaFc is the in deviation in the forces from the steady hover trim condition (1x1)
-% deltaGc is the in deviation in the moments from the steady hover trim condition (3x1)
-% Include attitude dynamics and kinematics using the Euler angle attitude representation. 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%  t: Time 
-%  var: 12x1 state vector   [ x, y, z, phi, theta, psi, u, v, w, p, q, r] 
-%  g: accel due to grav 
-%  m: Mass
-%  I: Inertia Matrix
-%  var_dot: 12x1 derivative of the state vector
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Inputs:   time = simulation time
+%           var = 12x1 state vector [x,y,z,phi,theta,psi,u,v,w,p,q,r] 
+%           g = acceleration due to gravity (m/s^2) 
+%           m = QR mass (kg)
+%           I = QR inertia matrix (km*m^2)
+%           deltaFc = Linearized distrubance in QR force Zc
+%           deltaGc = Linearized distrubance in QR moments (Lc,Mc,Nc)
+% 
+% Output:   var_dot: 12x1 derivative of the state vector
+%
+% Methodology: Using derivations from class, calculate the dotted state
+% vaiables given values for the previous step and using linearized formulas
+% and values. This function will be used by ode45 to simulate QR flight.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-State.x=var(1);
-State.y=var(2);
-State.z=var(3);
-State.phi=var(4);
-State.theta=var(5);
-State.psi=var(6);
-State.u=var(7);
-State.v=var(8);
-State.w=var(9);
-State.p=var(10);
-State.q=var(11);
-State.r=var(12);
+%% Assign Useful Names to Variables (x,y,z,phi,theta,etc...)
 
-%% Inertia Struct
-In.x=I(1,1);
-In.y=I(2,2);
-In.z=I(3,3);
+State.x=var(1); State.y=var(2); State.z=var(3);             % Position
+State.phi=var(4); State.theta=var(5); State.psi=var(6);     % Attitude
+State.u=var(7); State.v=var(8); State.w=var(9);             % Velocity
+State.p=var(10); State.q=var(11); State.r=var(12);          % Angular Rate
 
-%% Disturbances
+%% Extracting Inertia Values
+
+In.x = I(1,1); In.y = I(2,2); In.z = I(3,3);
+
+%% Extracting Disturbance Values
+
+% Force
 Disturb.Z = deltaFc;
-Disturb.L = deltaGc(1);
-Disturb.M = deltaGc(2);
-Disturb.N = deltaGc(3);
+
+% Moments
+Disturb.L = deltaGc(1); Disturb.M = deltaGc(2); Disturb.N = deltaGc(3);
 
 %% X_dot, Y_dot, Z_dot
-X_dot = State.u;
-Y_dot = State.v;
-Z_dot = State.w;
 
+X_dot = State.u; Y_dot = State.v; Z_dot = State.w;
 
 %% Phi_dot, Theta_dot, Psi_dot
-Phi_dot = State.p;
-Theta_dot = State.q;
-Psi_dot = State.r;
 
+Phi_dot = State.p; Theta_dot = State.q; Psi_dot = State.r;
 
 %% U_dot, V_dot, W_dot
-U_dot = -State.theta * g;
-V_dot = State.phi * g;
-W_dot = Disturb.Z / m;
 
+U_dot = -State.theta * g; V_dot = State.phi * g; W_dot = Disturb.Z / m;
 
 %% P_dot, Q_dot, R_dot
-P_dot = Disturb.L/In.x;
-Q_dot = Disturb.M/In.y;
-R_dot = Disturb.N/In.z;
+
+P_dot = Disturb.L/In.x; Q_dot = Disturb.M/In.y; R_dot = Disturb.N/In.z;
 
 %% Compilation
+
 var_dot=[ X_dot; Y_dot; Z_dot; Phi_dot; Theta_dot; Psi_dot; U_dot; V_dot; W_dot; P_dot; Q_dot; R_dot];
 
 end
