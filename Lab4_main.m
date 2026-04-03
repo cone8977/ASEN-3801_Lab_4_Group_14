a49f184f58c31e082d82e4b0bd6879d615eeef95
saki-07 <sagu4278@colorado.edu>
2026-04-03 14:55:26 -0600

    Add files via upload

--- Lab4_main.m
+++ Lab4_main.m
@@ -1,3 +1,10 @@
+% Contributors : Saketh Guttapalli, Cody Newton , Victor Turpin Aguayo , Liz Thompson
+% Course number: ASEN 3801
+% File name: Lab4_main.m
+% Created : 3/10/26
+
+
+
 clc 
 clear 
 close all 
@@ -5,7 +12,7 @@ close all
 t= [0 10];
 var=[10; 15 ;-20;
      0 ;0 ; pi/2; 
-     0 ;0 ;0;
+     -0.4 ; 1 ;-0.25;
      0 ;0 ;0];
 g=9.81;
 m=0.068;
@@ -20,6 +27,131 @@ motor_forces=[m*g/4 m*g/4 m*g/4 m*g/4]';
 
 
 %% function 
+function [var_dot, Control] = QuadrotorEOM(t, var, g, m, I, d, km, nu, mu, motor_forces)
+% for use by ode45 to simulate the full nonlinear equations of motion where: t is time; var is the 12 x
+% 1 aircraft state vector; g is the acceleration due to gravity; m is mass; I is the inertia matrix; d, km,
+% nu, and mu are the remaining quadrotor parameters; motor_forces = [f1; f2; f3; f4] is
+% the 4 x 1 vector of motor forces, and var_dot is the 12 x 1 derivative of the state vector. Include
+% attitude dynamics and kinematics using the Euler angle attitude representation. 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%  t: Time 
+%  var: 12x1 state vector   [ x, y, z, phi, theta, psi, u, v, w, p, q, r] 
+%  g: accel due to grav 
+%  m: Mass
+%  I: Inertia Matrix
+%  d: 
+%  km:
+%  nu:
+%  mu:
+%  Motor Forces: [f1; f2; f3; f4] is the 4 x 1 vector of motor forces
+%  var_dot: 12x1 derivative of the state vector
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+State.x=var(1);
+State.y=var(2);
+State.z=var(3);
+State.phi=var(4);
+State.theta=var(5);
+State.psi=var(6);
+State.u=var(7);
+State.v=var(8);
+State.w=var(9);
+State.p=var(10);
+State.q=var(11);
+State.r=var(12);
+%% Trig structure 
+% Psi
+Trig.cpsi=cos(State.psi);
+Trig.spsi=sin(State.psi);
+Trig.tpsi=tan(State.psi);
+% Theta
+Trig.ctheta=cos(State.theta);
+Trig.stheta=sin(State.theta);
+Trig.ttheta=tan(State.theta);
+% Phi
+Trig.cphi=cos(State.phi);
+Trig.sphi=sin(State.phi);
+Trig.tphi=tan(State.phi);
+
+%% Inetria Struct
+In.x=I(1,1);
+In.y=I(2,2);
+In.z=I(3,3);
+
+
+%% Control Struct
+control=[-1 -1 -1 -1; -d./sqrt(2) -d./sqrt(2) d./sqrt(2) d./sqrt(2); d./sqrt(2) -d./sqrt(2) -d./sqrt(2) d./sqrt(2); km -km km -km].*motor_forces;
+Control.Z=sum(control(1,:));
+Control.L=sum(control(2,:));
+Control.M=sum(control(3,:));
+Control.N=sum(control(4,:));
+
+%% Aero Struct
+Force= -nu.*norm([State.u State.v State.w]).*[State.u; State.v; State.w];
+Moments=-mu.*norm([State.p State.q State.r]).*[State.p; State.q; State.r];
+Aero.X=Force(1);
+Aero.Y=Force(2);
+Aero.Z=Force(3);
+Aero.L=Moments(1);
+Aero.M=Moments(2);
+Aero.N=Moments(3);
+
+%% X_dot, Y_dot, Z_dot
+Pos_dot=[Trig.ctheta.*Trig.cpsi, (Trig.sphi.*Trig.stheta.*Trig.cpsi)-(Trig.cphi.*Trig.spsi), (Trig.cphi.*Trig.stheta.*Trig.cpsi)-(Trig.sphi.*Trig.spsi); ...
+                     Trig.ctheta.*Trig.spsi, (Trig.sphi.*Trig.stheta.*Trig.spsi)+(Trig.cphi.*Trig.cpsi), (Trig.cphi.*Trig.stheta.*Trig.spsi)-(Trig.sphi.*Trig.cpsi); ...
+                     -Trig.stheta, Trig.ctheta.*Trig.sphi, Trig.ctheta.*Trig.cphi] * [State.u;State.v;State.w];
+
+X_dot = Pos_dot(1);
+Y_dot = Pos_dot(2);
+Z_dot = Pos_dot(3);
+
+
+%% Phi_dot, Theta_dot, Psi_dot
+Angle_dot=[ 1, Trig.sphi.*Trig.ttheta, Trig.cphi.*Trig.ttheta;
+            0, Trig.cphi, -Trig.sphi
+            0, Trig.sphi./Trig.ctheta, Trig.cphi./Trig.ctheta]*[State.p;State.q;State.r;]; 
+
+
+Phi_dot = Angle_dot(1);
+Theta_dot = Angle_dot(2);
+Psi_dot = Angle_dot(3);
+
+
+%% U_dot, V_dot, W_dot
+
+v_dot= cross([State.u,State.v,State.w],[State.p,State.q,State.r])' + g.*[-Trig.stheta;Trig.ctheta.*Trig.sphi ;Trig.ctheta.*Trig.cphi] +[Aero.X; Aero.Y; Aero.Z]./m+[0;0;Control.Z]./m;
+
+U_dot = v_dot(1);
+V_dot = v_dot(2);
+W_dot = v_dot(3);
+
+
+%% P_dot, Q_dot, R_dot
+Omega_dot= [ ((In.y-In.z)./In.x).*State.q.*State.r;((In.z-In.x)./In.y).*State.p.*State.r;((In.x-In.y)./In.z).*State.q.*State.p]+[(1/In.x).*Aero.L; (1/In.y).*Aero.M; (1/In.z).*Aero.N]+[(1/In.x).*Control.L; (1/In.y).*Control.M; (1/In.z).*Control.N];
+
+P_dot = Omega_dot(1);
+Q_dot = Omega_dot(2);
+R_dot = Omega_dot(3);
+
+% %% Ground Collison
+% if(State.z>=0)        
+%     Z_dot=0; 
+%     U_dot=0;
+%     V_dot=0;
+%     W_dot=0;
+%     P_dot=0;
+%     Q_dot=0;
+%     R_dot=0;
+% end
+
+%% Compilation
+var_dot=[ X_dot; Y_dot; Z_dot; Phi_dot; Theta_dot; Psi_dot; U_dot; V_dot; W_dot; P_dot; Q_dot; R_dot];
+
+
+end
+
+
+
+
 
 func=@(t,var) QuadrotorEOM(t, var, g, m, I, d, km, nu, mu, motor_forces);
 [T,y] = ode45(func,t,var);
@@ -36,4 +168,11 @@ for i = 1:length(T)
     N_c(i) = Control{i}.N;
 end
 
-PlotAircraftSim(T,y,[Z_c,L_c,M_c,N_c],[1 2 3 4 5 6],['b' 'b' 'b' 'b' 'b' 'b'])
+fig = 1:6;
+PlotAircraftSim(T,y,[Z_c,L_c,M_c,N_c],fig,['b'; 'b'; 'b'; 'b'; 'b'; 'b';'k'], '15')
+
+
+exportgraphics(figure(6),'3D_Trajectory_For_Velocity_deviation.png')
+exportgraphics(figure(4),'Body_Angular_Rates_Components_For_Velocity_deviation.png')
+exportgraphics(figure(2),'Body_Angular_Components_For_Velocity_deviation.png')
+exportgraphics(figure(1),'Inertial_Position_Components_For_Velocity_deviation.png')
